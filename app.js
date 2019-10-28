9159563afde58ed6f36d3ed58c4734d76297101d
harshit shukla <shuklaharshit516@gmail.com>
2019-10-28 23:11:53 +0530

    login and register handlebars added

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+//load routes
+const ideas = require("./routes/ideas");
+const users = require("./routes/users");
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
 // Connect to mongoose
 mongoose
     .connect("mongodb://localhost/vidjot-dev", {
-        useMongoClient: true,
-        useNewUrlParser: true,
-        useFindAndModify: false
+        useMongoClient: true
+        // useNewUrlParser: true,
+        // useFindAndModify: false
     })
     .then(() => console.log("MongoDB Connected..."))
     .catch(err => console.log(err));
@@ -37,6 +40,8 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//static folder
+app.use(express.static(__dirname + "/public"));
 // Method override middleware
 app.use(methodOverride("_method"));
 
@@ -49,6 +54,7 @@ app.use(
     })
 );
 
+//connect-flash middleware
 app.use(flash());
 
 // Global variables
@@ -72,85 +78,10 @@ app.get("/about", (req, res) => {
     res.render("about");
 });
 
-// Idea Index Page
-app.get("/ideas", (req, res) => {
-    Idea.find({})
-        .sort({ date: "desc" })
-        .then(ideas => {
-            res.render("ideas/index", {
-                ideas: ideas
-            });
-        });
-});
+//use routes
+app.use("/ideas", ideas);
 
-// Add Idea Form
-app.get("/ideas/add", (req, res) => {
-    res.render("ideas/add");
-});
-
-// Edit Idea Form
-app.get("/ideas/edit/:id", (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    }).then(idea => {
-        res.render("ideas/edit", {
-            idea: idea
-        });
-    });
-});
-
-// Process Form
-app.post("/ideas", (req, res) => {
-    let errors = [];
-
-    if (!req.body.title) {
-        errors.push({ text: "Please add a title" });
-    }
-    if (!req.body.details) {
-        errors.push({ text: "Please add some details" });
-    }
-
-    if (errors.length > 0) {
-        res.render("ideas/add", {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        });
-    } else {
-        const newUser = {
-            title: req.body.title,
-            details: req.body.details
-        };
-        new Idea(newUser).save().then(idea => {
-            req.flash("success_msg", "Video idea added");
-            res.redirect("/ideas");
-        });
-    }
-});
-
-// Edit Form process
-app.put("/ideas/:id", (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    }).then(idea => {
-        // new values
-        idea.title = req.body.title;
-        idea.details = req.body.details;
-
-        idea.save().then(idea => {
-            req.flash("success_msg", "Video idea updated");
-            res.redirect("/ideas");
-        });
-    });
-});
-
-// Delete Idea
-app.delete("/ideas/:id", (req, res) => {
-    Idea.remove({ _id: req.params.id }).then(() => {
-        req.flash("success_msg", "Video idea removed");
-        res.redirect("/ideas");
-    });
-});
+app.use("/users", users);
 
 const port = 5000;
 
