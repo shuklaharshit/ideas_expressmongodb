7516235414466a5cb6032ec06c9bd4bc27db27bd
harshit shukla <37479246+harshit516@users.noreply.github.com>
2019-10-30 15:50:24 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 if (process.env.NODE_ENV === "production") {
     module.exports = {
         mongoURI:
-            "mongodb://ideas516:Harshit%401@cluster0-shard-00-00-rfgwz.mongodb.net:27017,cluster0-shard-00-01-rfgwz.mongodb.net:27017,cluster0-shard-00-02-rfgwz.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+            "ur database"
     };
 } else {
     module.exports = {
         mongoURI: "mongodb://localhost/vidjot-dev"
-        // "mongodb://ideas516:Harshit%401@cluster0-shard-00-00-rfgwz.mongodb.net:27017,cluster0-shard-00-01-rfgwz.mongodb.net:27017,cluster0-shard-00-02-rfgwz.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+   
     };
 }
